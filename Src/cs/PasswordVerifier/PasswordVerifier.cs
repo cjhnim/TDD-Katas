6c832d776ff64e99699727515720ac1402ef8de5
cjhnim <cjhnim@gmail.com>
2017-02-09 17:54:35 +0900

    PasswordVerifier Step1
    from: http://osherove.com/tdd-kata-3-refactoring/

---
+++ Src/cs/PasswordVerifier/PasswordVerifier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Katas_project.PasswordVerifier
+{
+    
+    class PasswordVerifier
+    {
+        private string Password;
+
+        internal void Verify()
+        {
+            if (Password == null)
+            {
+                throw new PasswordVerifierException("password should not be null");
+            }
+            if (Password.Length <= 8)
+            {
+                throw new PasswordVerifierException("password should be larger than 8 chars");
+            }
+            if (!Password.Any(char.IsUpper)) //Todo: 문법이해하자
+            {
+                throw new PasswordVerifierException("password should have one uppercase letter at least");
+            }
+            if (!Password.Any(char.IsLower)) //Todo: 문법이해하자
+            {
+                throw new PasswordVerifierException("password should have one lowercase letter at least");
+            }
+            if (!Password.Any(char.IsNumber)) //Todo: 문법이해하자
+            {
+                throw new PasswordVerifierException("password should have one number at least");
+            }
+        }
+
+        internal void InputPassword(string password)
+        {
+            Password = password;
+        }
+    }
+}
---
+++ Src/cs/PasswordVerifier/PasswordVerifierException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Katas_project.PasswordVerifier
+{
+    [Serializable()]
+    public class PasswordVerifierException : System.Exception
+    {
+        public PasswordVerifierException() : base() { }
+        public PasswordVerifierException(string message) : base(message) { }
+        public PasswordVerifierException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected PasswordVerifierException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        { }
+    }
+}
