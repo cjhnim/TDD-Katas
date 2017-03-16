ef5f2110a123360e512fa1e83b13096625441c6f
choi jung hyun <cjhnim@gmail.com>
2017-03-16 09:15:51 +0900

    I'm solved the minefield kata

--- Src/cs/MineFields/TestMineFields.cs
+++ Src/cs/MineFields/TestMineFields.cs
@@ -30,9 +30,172 @@ namespace TDD_Katas_project.MineFields
     class TestMineFields
     {
         [Test]
-        public void simpleTest()
+        public void build_field_size_of_one_by_one_and_zero_mine()
         {
-            Assert.Fail();
+            MineFields fields = new MineFields();
+
+            fields.create(1, 1);
+
+            Assert.That(fields.getHint(0, 0), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void build_field_size_of_one_by_one_and_one_mine()
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(1, 1);
+            fields.mine(0, 0);
+
+            Assert.That(fields.getHint(0, 0), Is.EqualTo(-1));
+        }
+
+        [Test]
+        [TestCase(0, 0, -1)]
+        [TestCase(1, 0, 1)]
+        [TestCase(0, 1, 1)]
+        [TestCase(1, 1, 1)]
+        public void build_two_by_two_field_and_one_of_mine_topleftcorner(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(0, 0);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, 1)]
+        [TestCase(1, 0, 1)]
+        [TestCase(0, 1, 1)]
+        [TestCase(1, 1, Constants.Mine)]
+        public void build_two_by_two_field_and_one_of_mine_bottomrightcorner(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(1, 1);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, 1)]
+        [TestCase(1, 0, 1)]
+        [TestCase(0, 1, Constants.Mine)]
+        [TestCase(1, 1, 1)]
+        public void build_two_by_two_field_and_one_of_mine_bottomleftcorner(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(0, 1);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, 1)]
+        [TestCase(1, 0, Constants.Mine)]
+        [TestCase(0, 1, 1)]
+        [TestCase(1, 1, 1)]
+        public void build_two_by_two_field_and_one_of_mine_toprightcorner(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(1, 0);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, Constants.Mine)]
+        [TestCase(1, 0, Constants.Mine)]
+        [TestCase(0, 1, 2)]
+        [TestCase(1, 1, 2)]
+        public void build_two_by_two_field_and_two_of_mine(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(0, 0);
+            fields.mine(1, 0);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, Constants.Mine)]
+        [TestCase(1, 0, Constants.Mine)]
+        [TestCase(0, 1, Constants.Mine)]
+        [TestCase(1, 1, 3)]
+        public void build_two_by_two_field_and_three_of_mine(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(0, 0);
+            fields.mine(1, 0);
+            fields.mine(0, 1);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, Constants.Mine)]
+        [TestCase(1, 0, Constants.Mine)]
+        [TestCase(0, 1, Constants.Mine)]
+        [TestCase(1, 1, Constants.Mine)]
+        public void build_two_by_two_field_and_four_of_mine(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(2, 2);
+            fields.mine(0, 0);
+            fields.mine(1, 0);
+            fields.mine(0, 1);
+            fields.mine(1, 1);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, -1)][TestCase(1, 0, -1)][TestCase(2, 0, 1)]
+        [TestCase(0, 1, 2)][TestCase(1, 1, 2)][TestCase(2, 1, 1)]
+        [TestCase(0, 2, 0)][TestCase(1, 2, 0)][TestCase(2, 2, 0)]
+        public void build_three_by_three_field_and_two_of_mine(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(3, 3);
+            fields.mine(0, 0);
+            fields.mine(1, 0);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
+        }
+
+        [Test]
+        [TestCase(0, 0, Constants.Mine)]
+        [TestCase(1, 0, Constants.Mine)]
+        [TestCase(2, 0, 1)]
+        [TestCase(0, 1, 3)]
+        [TestCase(1, 1, 3)]
+        [TestCase(2, 1, 2)]
+        [TestCase(0, 2, 1)]
+        [TestCase(1, 2, -1)]
+        [TestCase(2, 2, 1)]
+        public void build_three_by_three_field_and_three_of_mine(int xPos, int yPos, int expect)
+        {
+            MineFields fields = new MineFields();
+
+            fields.create(3, 3);
+            fields.mine(0, 0);
+            fields.mine(1, 0);
+            fields.mine(1, 2);
+
+            Assert.That(fields.getHint(xPos, yPos), Is.EqualTo(expect));
         }
     }
 }
